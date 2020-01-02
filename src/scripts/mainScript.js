a3b343107698793a61a419b7484780959dad2f86
Teo Winkler <teo.winkler@gmail.com>
2020-01-03 00:02:19 +0100

    added showcase photo and fixed soem bugs

--- src/scripts/mainScript.js
+++ src/scripts/mainScript.js
@@ -1,14 +1,14 @@
-let menuButton = document.getElementById("menu-button");
-let closeButton = document.getElementById("close-button");
-
-let racesButton = document.getElementById("races-button");
-let dropdownMenu = document.getElementById("dropdown");
-
-let inputFields = document.querySelectorAll(".input-field");
-
-
 //check if doc is loaded
 document.addEventListener('DOMContentLoaded', function () {
+    let menuButton = document.getElementById("menu-button");
+    let closeButton = document.getElementById("close-button");
+
+    let racesButton = document.getElementById("races-button");
+    let dropdownMenu = document.getElementById("dropdown");
+
+    let inputFields = document.querySelectorAll(".input-field");
+
+    let submitButton = document.getElementById("submit-button");
 
     //open & close menu
     function openMenu() {
@@ -27,21 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
-    menuButton.addEventListener("click", openMenu)
-    closeButton.addEventListener("click", closeMenu)
-    racesButton.addEventListener("click", racesMenu)
-
-    //button listener
-    document.addEventListener("click", function (e) {
-        if (e.target.classList.contains("hide-menu")) {
-            closeMenu();
-        }
-    
-        //scroll back on top and close menu if on the same page
-
-    
-    });
-
     //clear input fields on reload
     window.onload = function() {
         inputFields.forEach (function(field) {
@@ -50,5 +35,35 @@ document.addEventListener('DOMContentLoaded', function () {
         
     };
 
+    // validate form
+
+
+    //button listeners
+    menuButton.addEventListener("click", openMenu);
+    closeButton.addEventListener("click", closeMenu);
+    racesButton.addEventListener("click", racesMenu);
+    //submitButton.addEventListener("click", validateForm);
+});
+
+
+// jQuery
+$(document).ready(function () {
+    let width = $(window).width();
+    $(window).resize(function() {
+        width = $(window).width()
+    });    
+
+    $('#races-button').click(function(){    
+        // scroll to races
+        if (width >= 700) { 
+            // index.html
+            if (window.location.href == "http://127.0.0.1:5500/build/index.html") {
+                $('html, body').animate({scrollTop:$('#section-a').position().top-95}, 'slow');
+            }
+            // non-index pages
+        };
+    });
+
+    //validate form
 
 });
