d12feaa79223f25a1554d1c0a9cdf28623241dd5
Teo Winkler <teo.winkler@gmail.com>
2020-01-06 23:24:39 +0100

    updated gitignore

--- src/scripts/mainScript.js
+++ src/scripts/mainScript.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
     let racesButton = document.getElementById("races-button");
     let dropdownMenu = document.getElementById("dropdown");
 
-    let inputFields = document.querySelectorAll(".input-field");
-
     // old validation
     let name = document.getElementById("name");
     let email = document.getElementById("email");
@@ -30,44 +28,6 @@ $(document).ready(function () {
         }
     };
 
-    function validate() {
-        let nameValue = name.value.trim();
-        let emailValue = email.value.trim();
-        let messageValue = message.value.trim();
-        
-        if (nameValue === '') {
-            error();
-        } else {
-            success()
-        }
-        
-        if (emailValue === '') {
-            error()
-        } else if (!isEmail(emailValue)) {
-            error()
-        } else {
-            success()
-        }
-        
-        if (messageValue === '') {
-            error()
-        } else {
-            success() 
-        }
-    }
-
-    function error() {
-        return false
-    }
-    function success(){
-        return true
-    }
-        
-    function isEmail(email) {
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-    }
-
-
     // button listeners
     menuButton.addEventListener("click", openMenu);
     closeButton.addEventListener("click", closeMenu);
@@ -83,12 +43,8 @@ $(document).ready(function () {
         if (width >= 700) { 
             // index.html
             if (window.location.href == "http://127.0.0.1:5500/build/index.html") {
-                $('html, body').animate({scrollTop:$('#section-a').position().top-80}, 'slow');
+                $('html, body').animate({scrollTop:$('#section-a').position().top}, 'slow');
             }
-            // non-index pages
-            // ??? figure out
-
-
         };
     });
 
